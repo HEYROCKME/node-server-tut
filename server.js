0d37ed0a9145806238bd6c1259cee1de0197f8ef
Alwin Nedrum <nedrum@gmail.com>
2024-06-19 11:54:02 +0200

    set up minimal server

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+const fsPromises = require('fs').promises
+
 const logEvents = require('./logEvents')
 const EventEmiter = require('events')
 
@@ -7,11 +12,16 @@ class Emitter extends EventEmiter {}
 
 const myEmitter = new Emitter()
 
+const PORT = process.env.PORT || 3500
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method)
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
 // add listener
+// myEmitter.on('log', (msg) => logEvents(msg))
 
-myEmitter.on('log', (msg) => logEvents(msg))
-
-setTimeout(() => {
-  // Emit event
-  myEmitter.emit('log', 'Log event emitted')
-}, 2000)
+// Emit event
+//  myEmitter.emit('log', 'Log event emitted')
