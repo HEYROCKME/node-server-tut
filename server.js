4199c930f062b95a0859ba808c41937944e28de6
Alwin Nedrum <nedrum@gmail.com>
2024-06-19 12:09:07 +0200

    set content type

--- server.js
+++ server.js
@@ -14,8 +14,38 @@ const myEmitter = new Emitter()
 
 const PORT = process.env.PORT || 3500
 
+// Create a server
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method)
+
+  const extension = path.extname(req.url)
+
+  // Setting the Content Type
+  let contentType
+
+  switch (extension) {
+    case '.css':
+      contentType = 'text/css'
+      break
+    case '.js':
+      contentType = 'text/javascript'
+      break
+    case '.json':
+      contentType = 'application/json'
+      break
+    case '.jpg':
+      contentType = 'image/jpeg'
+      break
+    case '.png':
+      contentType = 'image/png'
+      break
+    case '.txt':
+      contentType = 'text/plain'
+      break
+
+    default:
+      contentType = 'text/html'
+  }
 })
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
