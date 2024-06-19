8cfb8400cc0efa763ce283411b2718cce968261b
Alwin Nedrum <nedrum@gmail.com>
2024-06-19 15:03:48 +0200

    basic return of websites

--- server.js
+++ server.js
@@ -14,6 +14,18 @@ const myEmitter = new Emitter()
 
 const PORT = process.env.PORT || 3500
 
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const data = await fsPromises.readFile(filePath, 'utf8')
+    response.writeHead(200, { ContentType: contentType })
+    response.end(data)
+  } catch (err) {
+    console.error(err)
+    response.stausCode = 500
+    response.end()
+  }
+}
+
 // Create a server
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method)
@@ -45,6 +57,44 @@ const server = http.createServer((req, res) => {
 
     default:
       contentType = 'text/html'
+  }
+  // set file path according to url and content type
+  let filePath =
+    contentType === 'text/html' && req.url === '/'
+      ? path.join(__dirname, 'views', 'index.html')
+      : contentType === 'text/html' && req.url.slice(-1) === '/'
+      ? path.join(__dirname, 'views', req.url, 'index.html')
+      : contentType === 'text/html'
+      ? path.join(__dirname, 'views', req.url)
+      : path.join(__dirname, req.url)
+
+  // Makes the .html not required in the browser
+  if (!extension && req.url.slice(-1) !== '/') filePath += '.html'
+
+  // Check if file exists
+  const fileExists = fs.existsSync(filePath)
+
+  if (fileExists) {
+    // serve the file
+    serveFile(filePath, contentType, res)
+  } else {
+    switch (path.parse(filePath).base) {
+      case 'old-page.html':
+        res.writeHead(301, { location: '/new-page.html' })
+        res.end()
+        break
+      case 'www-page.html':
+        res.writeHead(301, { location: '/' })
+        res.end()
+        break
+
+      default:
+        serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res)
+      //   break;
+    }
+    // 404
+    // 301 redirect
+    // console.log(path.parse(filePath))
   }
 })
 
